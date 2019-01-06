0cb3d9a0e9540c88e7c9710a748e308ab53849c8
Full Stack Technology <38210356+fulstat@users.noreply.github.com>
2019-01-06 07:26:55 +0700

    Create Colour_Selection.py

---
+++ Colour_Selection.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+# Read in the image and print out some stats
+image = mpimg.imread('Resource/test.jpg')
+print('This image is: ',type(image), 
+         'with dimensions:', image.shape)
+
+# Grab the x and y size and make a copy of the image
+ysize = image.shape[0]
+xsize = image.shape[1]
+# Note: always make a copy rather than simply using "="
+color_select = np.copy(image)
+
+# Define our color selection criteria
+# Note: if you run this code, you'll find these are not sensible values
+# #000000
+red_threshold = 0
+green_threshold = 0
+blue_threshold = 0
+rgb_threshold = [red_threshold, green_threshold, blue_threshold]
+
+# Identify pixels below the threshold
+thresholds = (image[:,:,0] < rgb_threshold[0]) \
+            | (image[:,:,1] < rgb_threshold[1]) \
+            | (image[:,:,2] < rgb_threshold[2])
+color_select[thresholds] = [0,0,0]
+
+# Display the image                 
+plt.imshow(color_select)
+plt.show()
+
