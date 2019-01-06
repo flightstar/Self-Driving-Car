c122da03b9a1c52a04aadb13766a1e552320779a
Full Stack Technology <38210356+fulstat@users.noreply.github.com>
2019-01-06 13:58:08 +0700

    Create Canny_Algorithm_Detect_Land_Line.py

---
+++ Canny_Algorithm_Detect_Land_Line.py
@@ -0,0 +1,25 @@
+#doing all the relevant imports
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
+
+# Read in the image and convert to grayscale
+image = mpimg.imread('exit-ramp.jpg')
+gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+# Define a kernel size for Gaussian smoothing / blurring
+# Note: this step is optional as cv2.Canny() applies a 5x5 Gaussian internally
+kernel_size = 3
+# Applying Canny to the image gray
+blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size), 0)
+
+# Define parameters for Canny and run it
+# NOTE: if you try running this code you might want to change these!
+# Ratio of low_threshold to high_threshold, John Canny recommended a low to high ratio of 1:2 or 1:3.
+low_threshold = 1
+high_threshold = 10
+edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+
+# Display the image
+plt.imshow(edges, cmap='Greys_r')
