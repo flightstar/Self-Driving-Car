a393cc60fb06a100d6f05f46f3252da94c609ba4
Full Stack Technology <38210356+fulstat@users.noreply.github.com>
2019-01-06 14:09:40 +0700

    Update Canny_Algorithm_Detect_Land_Line.py

--- Canny_Algorithm_Detect_Land_Line.py
+++ Canny_Algorithm_Detect_Land_Line.py
@@ -10,6 +10,7 @@ gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
 # Define a kernel size for Gaussian smoothing / blurring
 # Note: this step is optional as cv2.Canny() applies a 5x5 Gaussian internally
+# Must be an odd number (3, 5, 7...)
 kernel_size = 3
 # Applying Canny to the image gray
 blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size), 0)
@@ -17,8 +18,8 @@ blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size), 0)
 # Define parameters for Canny and run it
 # NOTE: if you try running this code you might want to change these!
 # Ratio of low_threshold to high_threshold, John Canny recommended a low to high ratio of 1:2 or 1:3.
-low_threshold = 1
-high_threshold = 10
+low_threshold = 100
+high_threshold = 200
 edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
 
 # Display the image
