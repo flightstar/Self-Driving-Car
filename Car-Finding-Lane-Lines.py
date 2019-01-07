8de296f5276c97bb9c6f9c92c3d3944ff8ae6c2d
Full Stack Technology <38210356+fulstat@users.noreply.github.com>
2019-01-07 07:54:23 +0700

    Update Car-Finding-Lane-Lines.py

--- Car-Finding-Lane-Lines.py
+++ Car-Finding-Lane-Lines.py
@@ -5,8 +5,65 @@ import cv2
 from moviepy.editor import *
 from collections import deque
 
-QUEUE_LENGTH=50
+def select_white_yellow(image):
+    converted = convert_hls(image)
+    # white color mask
+    lower = np.uint8([  0, 200,   0])
+    upper = np.uint8([255, 255, 255])
+    white_mask = cv2.inRange(converted, lower, upper)
+    # yellow color mask
+    lower = np.uint8([ 10,   0, 100])
+    upper = np.uint8([ 40, 255, 255])
+    yellow_mask = cv2.inRange(converted, lower, upper)
+    # combine the mask
+    mask = cv2.bitwise_or(white_mask, yellow_mask)
+    return cv2.bitwise_and(image, image, mask = mask)
 
+def convert_gray_scale(image):
+    return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+def apply_smoothing(image, kernel_size=15):
+#   Kernel_size must be postivie and odd
+    return cv2.GaussianBlur(image, (kernel_size, kernel_size), 0)
+
+def detect_edges(image, low_threshold=50, high_threshold=150):
+    return cv2.Canny(image, low_threshold, high_threshold)
+
+def select_region(image):
+    # first, define the polygon by vertices
+    # image.shape[0]: y, image.shape[1]: x
+    rows, cols = image.shape[:2]
+    bottom_left  = [cols*0.1, rows*0.95]
+    top_left     = [cols*0.4, rows*0.6]
+    bottom_right = [cols*0.9, rows*0.95]
+    top_right    = [cols*0.6, rows*0.6] 
+    # the vertices are an array of polygons (i.e array of arrays) and the data type must be integer
+    vertices = np.array([[bottom_left, top_left, top_right, bottom_right]], dtype=np.int32)
+    return filter_region(image, vertices)
+
+def hough_lines(image):  
+#   Returns hough lines (not the image with lines)
+    return cv2.HoughLinesP(image, rho=1, theta=np.pi/180, threshold=20, minLineLength=20, maxLineGap=300)
+
+def lane_lines(image, lines):
+    left_lane, right_lane = average_slope_intercept(lines)
+    y1 = image.shape[0] # bottom of the image
+    y2 = y1*0.6         # slightly lower than the middle
+    left_line  = make_line_points(y1, y2, left_lane)
+    right_line = make_line_points(y1, y2, right_lane)    
+    return left_line, right_line
+
+def draw_lane_lines(image, lines, color=[255, 0, 0], thickness=20):
+    # make a separate image to draw lines and combine with the orignal later
+    line_image = np.zeros_like(image)
+    for line in lines:
+        if line is not None:
+            cv2.line(line_image, *line,  color, thickness)
+    # image1 * α + image2 * β + λ
+    # image1 and image2 must be the same shape.
+    return cv2.addWeighted(image, 1.0, line_image, 0.95, 0.0)
+
+QUEUE_LENGTH=50
 class LaneDetector:
     def __init__(self):
         self.left_lines  = deque(maxlen=QUEUE_LENGTH)
