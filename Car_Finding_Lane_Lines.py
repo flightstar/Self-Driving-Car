2db5c9c5b6f0d2cccbc73fb28ffdbc74426d71b5
Full Stack Technology <38210356+fulstat@users.noreply.github.com>
2019-01-07 12:00:02 +0700

    Update Car_Finding_Lane_Lines.py

--- Car_Finding_Lane_Lines.py
+++ Car_Finding_Lane_Lines.py
@@ -48,6 +48,45 @@ def hough_lines(image):
 #   Returns hough lines (not the image with lines)
     return cv2.HoughLinesP(image, rho=1, theta=np.pi/180, threshold=20, minLineLength=20, maxLineGap=300)
 
+def average_slope_intercept(lines):
+    left_lines    = [] # (slope, intercept)
+    left_weights  = [] # (length,)
+    right_lines   = [] # (slope, intercept)
+    right_weights = [] # (length,)
+    
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            if x2==x1:
+                continue # ignore a vertical line
+            slope = (y2-y1)/(x2-x1)
+            intercept = y1 - slope*x1
+            length = np.sqrt((y2-y1)**2+(x2-x1)**2)
+            if slope < 0: # y is reversed in image
+                left_lines.append((slope, intercept))
+                left_weights.append((length))
+            else:
+                right_lines.append((slope, intercept))
+                right_weights.append((length))
+    
+    # add more weight to longer lines    
+    left_lane  = np.dot(left_weights,  left_lines) /np.sum(left_weights)  if len(left_weights) >0 else None
+    right_lane = np.dot(right_weights, right_lines)/np.sum(right_weights) if len(right_weights)>0 else None
+    
+    return left_lane, right_lane # (slope, intercept), (slope, intercept)
+
+def make_line_points(y1, y2, line):
+#   Convert a line represented in slope and intercept into pixel point
+    if line is None:
+        return None
+    slope, intercept = line    
+    # make sure everything is integer as cv2.line requires it
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    y1 = int(y1)
+    y2 = int(y2)
+    
+    return ((x1, y1), (x2, y2))
+
 def lane_lines(image, lines):
     left_lane, right_lane = average_slope_intercept(lines)
     y1 = image.shape[0] # bottom of the image
